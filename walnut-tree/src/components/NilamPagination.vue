be9afcceb984152509ea07ef39cd7fbe130ada65
ataurAGI <ataur.rahman@anwargroup.com>
2024-10-20 11:06:30 +0600

    pagination component dropdown style added

--- walnut-tree/src/components/NilamPagination.vue
+++ walnut-tree/src/components/NilamPagination.vue
@@ -1,71 +1,95 @@
 <template>
-    <div class="pagination-container">
-      <div class="pagination-info">
+  <div class="pagination-container">
+    <v-row class="pagination-row">
+      <v-col cols="3 mx-2">
         <span>Items per page:</span>
         <select class="per-page-dropdown" :value="perPage" @change="updatePerPage">
-          <option v-for="option in perPageOptions" :value="option" :key="option">{{ option }}</option>
+          <option
+            class="option-class"
+            v-for="option in perPageOptions"
+            :value="option"
+            :key="option"
+          >
+            {{ option }}
+          </option>
         </select>
+      </v-col>
+      <v-col cols="4" class="mx-auto">
         <span class="total-items-info">Total items: {{ totalItems }}</span>
-      </div>
-      <div class="pagination-controls">
-        <v-btn  size="small" @click="getPage('previous')" :disabled="currentPage === 1">Previous</v-btn>
-        <v-btn  size="small" @click="getPage('next')" :disabled="currentPage === totalPages">Next</v-btn>
-      </div>
-    </div>
-  </template>
-  
-  <style scoped>
-  .pagination-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between; 
-    margin-top: 20px;
-    align-items: center;
-  }
-  .pagination-controls button {
-    padding: 8px 12px;
-    background-color: #535553;
-    color: #b7f3a4;
-    border: none;
-    border-radius: 4px;
-    text-align: center;
-    cursor: pointer;
-    margin-right: 10px;
-  }
-  .per-page-dropdown {
-    padding: 8px;
-    margin: 8px;
-  }
-  .total-items-info {
-    margin-left: 10px;
-  }
-  .pagination-controls {
-    margin: 0 0 0 10px;
-  }
-  </style>
-  
-  <script setup lang="ts">
-  import { defineProps, defineEmits, computed } from 'vue';
-  
-  const props = defineProps<{
-    perPage: number;
-    totalItems: number;
-    currentPage: number;
-    perPageOptions: number[];
-  }>();
-  
-  const emit = defineEmits<{
-    (e: 'pagination-action', value: 'previous' | 'next'): void;
-    (e: 'updatePerPage', value: number): void;
-  }>();
-  const totalPages = computed(() => Math.ceil(props.totalItems / props.perPage));
-  
-  const getPage = (value: 'previous' | 'next') => {
-    emit('pagination-action', value);
-  };
-  const updatePerPage = (event: Event) => {
-    const selectedValue = Number((event.target as HTMLSelectElement).value);
-    emit('updatePerPage', selectedValue);
-  };
-  </script>
-  
+      </v-col>
+      <v-col cols="4" class="pagination-btn-group">
+        <v-btn size="small" @click="getPage('previous')" :disabled="currentPage === 1"
+          >Previous</v-btn
+        >
+        <v-btn size="small" @click="getPage('next')" :disabled="currentPage === totalPages"
+          >Next</v-btn
+        >
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<style scoped>
+.pagination-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 20px;
+  align-items: center;
+}
+.pagination-controls button {
+  padding: 8px 12px;
+  background-color: 'primary';
+  color: 'secondary';
+  border: none;
+  border-radius: 4px;
+  text-align: center;
+  cursor: pointer;
+  margin-right: 10px;
+}
+.per-page-dropdown {
+  padding: 8px;
+  margin: 8px;
+  border: 1px solid white;
+  color: white;
+}
+.option-class {
+  background-color: white;
+  color: black;
+}
+.total-items-info {
+  margin-left: 10px;
+}
+.pagination-btn-group {
+  display: flex;
+  justify-content: space-evenly;
+}
+.pagination-row {
+  align-items: center;
+}
+</style>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, computed } from 'vue'
+
+const props = defineProps<{
+  perPage: number
+  totalItems: number
+  currentPage: number
+  perPageOptions: number[]
+}>()
+
+const emit = defineEmits<{
+  (e: 'pagination-action', value: 'previous' | 'next'): void
+  (e: 'updatePerPage', value: number): void
+}>()
+const totalPages = computed(() => Math.ceil(props.totalItems / props.perPage))
+
+const getPage = (value: 'previous' | 'next') => {
+  emit('pagination-action', value)
+}
+const updatePerPage = (event: Event) => {
+  const selectedValue = Number((event.target as HTMLSelectElement).value)
+  emit('updatePerPage', selectedValue)
+}
+</script>
--- walnut-tree/src/plugins/vuetify.ts
+++ walnut-tree/src/plugins/vuetify.ts
@@ -3,83 +3,83 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 const artisticLightTheme = {
-    dark: false,
-    colors: {
-        background: '#FFFFFF',  
-        surface: '#02A676',  
-        'surface-bright': '#FAFAFA', 
-        'surface-light': '#F0F0F0', 
-        'surface-variant': '#E0E0E0', 
-        'on-surface-variant': '#424242', 
-        primary: '#FF7A73',  
-        'primary-darken-1': '#E86A63', 
-        secondary: '#8AB8DC', 
-        'secondary-darken-1': '#6D9DC5',  
-        error: '#FF6F61',  
-        info: '#42A5F5',  
-        success: '#66BB6A',  
-        warning: '#FFD54F',  
-    },
-    variables: {
-        'border-color': '#DDDDDD',  
-        'border-opacity': 0.15,
-        'high-emphasis-opacity': 0.87,
-        'medium-emphasis-opacity': 0.60,
-        'disabled-opacity': 0.38,
-        'hover-opacity': 0.1,
-        'focus-opacity': 0.15,
-        'selected-opacity': 0.1,
-        'pressed-opacity': 0.15,
-        'dragged-opacity': 0.08,
-        'theme-kbd': '#F0F0F0',  
-        'theme-on-kbd': '#000000',  
-        'theme-code': '#F5F5F5',  
-        'theme-on-code': '#000000', 
-    }
-};
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#02A676',
+    'surface-bright': '#FAFAFA',
+    'surface-light': '#F0F0F0',
+    'surface-variant': '#E0E0E0',
+    'on-surface-variant': '#424242',
+    primary: '#FF7A73',
+    'primary-darken-1': '#E86A63',
+    secondary: '#8AB8DC',
+    'secondary-darken-1': '#6D9DC5',
+    error: '#FF6F61',
+    info: '#42A5F5',
+    success: '#66BB6A',
+    warning: '#FFD54F'
+  },
+  variables: {
+    'border-color': '#DDDDDD',
+    'border-opacity': 0.15,
+    'high-emphasis-opacity': 0.87,
+    'medium-emphasis-opacity': 0.6,
+    'disabled-opacity': 0.38,
+    'hover-opacity': 0.1,
+    'focus-opacity': 0.15,
+    'selected-opacity': 0.1,
+    'pressed-opacity': 0.15,
+    'dragged-opacity': 0.08,
+    'theme-kbd': '#F0F0F0',
+    'theme-on-kbd': '#000000',
+    'theme-code': '#F5F5F5',
+    'theme-on-code': '#000000'
+  }
+}
 
 const artisticDarkTheme = {
-    dark: true,  
-    colors: {
-        background: '#121212',  
-        surface: '#003840',  
-        'surface-bright': '#2C353C',  
-        'surface-light': '#3A3C40',  
-        'surface-variant': '#4C545C',  
-        'on-surface-variant': '#FFFFFF',  
-        primary: '#66C6C2',  
-        'primary-darken-1': '#45B7B1', 
-        secondary: '#E6C77C',  
-        'secondary-darken-1': '#D9AA30',  
-        error: '#FF3D00', 
-        info: '#00BFFF',  
-        success: '#4CAF50',  
-        warning: '#FFAA00',  
-    },
-    variables: {
-        'border-color': '#CCCCCC',  
-        'border-opacity': 0.25,
-        'high-emphasis-opacity': 0.87,
-        'medium-emphasis-opacity': 0.60,
-        'disabled-opacity': 0.38,
-        'hover-opacity': 0.1,
-        'focus-opacity': 0.15,
-        'selected-opacity': 0.1,
-        'pressed-opacity': 0.15,
-        'dragged-opacity': 0.08,
-        'theme-kbd': '#212529',  
-        'theme-on-kbd': '#FFFFFF',  
-        'theme-code': '#F5F5F5',  
-        'theme-on-code': '#000000',  
-    }
-};
+  dark: true,
+  colors: {
+    background: '#121212',
+    surface: '#003840',
+    'surface-bright': '#2C353C',
+    'surface-light': '#3A3C40',
+    'surface-variant': '#4C545C',
+    'on-surface-variant': '#FFFFFF',
+    primary: '#66C6C2',
+    'primary-darken-1': '#45B7B1',
+    secondary: '#E6C77C',
+    'secondary-darken-1': '#D9AA30',
+    error: '#FF3D00',
+    info: '#00BFFF',
+    success: '#4CAF50',
+    warning: '#FFAA00'
+  },
+  variables: {
+    'border-color': '#CCCCCC',
+    'border-opacity': 0.25,
+    'high-emphasis-opacity': 0.87,
+    'medium-emphasis-opacity': 0.6,
+    'disabled-opacity': 0.38,
+    'hover-opacity': 0.1,
+    'focus-opacity': 0.15,
+    'selected-opacity': 0.1,
+    'pressed-opacity': 0.15,
+    'dragged-opacity': 0.08,
+    'theme-kbd': '#212529',
+    'theme-on-kbd': '#FFFFFF',
+    'theme-code': '#F5F5F5',
+    'theme-on-code': '#000000'
+  }
+}
 
 export default createVuetify({
-    theme: {
-      defaultTheme: 'artisticLightTheme',
-      themes: {
-        artisticLightTheme,
-        artisticDarkTheme
-      },
-    },
-});
+  theme: {
+    defaultTheme: 'artisticLightTheme',
+    themes: {
+      artisticLightTheme,
+      artisticDarkTheme
+    }
+  }
+})
